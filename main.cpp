752f4a130e19d534e678ae654fe84669ac4fd9f2
PCristian00 <cristianp9300@gmail.com>
2022-01-31 19:03:11 +0100

    Concatena, accenno di main

--- main.cpp
+++ main.cpp
@@ -10,6 +10,12 @@ char nome2[16];
 // due stringhe lette rispettivamente al primo ed al secondo argomento;
 // restituisce il puntatore al primo elemento della stringa generata
 char* concatena(char* s1, char* s2) {
+char* cat=new char[32];
+char* s=cat;
+while (*s1) {*s = *s1; s++; s1++;};
+    while (*s2) {*s = *s2; s++; s2++;};
+    *s = '\0';
+    return cat;
 
 }
 
@@ -32,5 +38,25 @@ bool CopiaStringa(fstream& fout, fstream& fin, char* s) {
 // due files aperti in lettura; per svolgere questo compito il programma
 // utilizza le funzioni CopiaStringa, LeggiStringa e strcmp
 int main() {
+
+    cout<<"Inserire nome file 1:"<<endl;
+    cin>>nome1;
+
+    cout<<"Inserire nome file 2:"<<endl;
+    cin>>nome2;
+
+    fin1.open(nome1,ios::in);
+    if(!fin1.is_open()) cout<<"Errore apertura "<<nome1<<endl;
+
+    fin2.open(nome2,ios::in);
+    if(!fin2.is_open()) cout<<"Errore apertura "<<nome2<<endl;
+
+    char *nome3[32];
+    *nome3 = concatena(nome1, nome2);
+    cout<<"Concat: "<<*nome3<<endl;
+
+    fileout.open(*nome3,ios::out);
+    if(!fileout.is_open()) cout<<"Errore apertura "<<*nome3<<endl;
+
 
 }
