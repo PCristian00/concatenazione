33076d4c0f343176cd9d86de48ec280f328c5120
PCristian00 <cristianp9300@gmail.com>
2022-02-01 15:51:28 +0100

    Funzionante ma incompleto

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <fstream>
 #include <iostream>
+#include <cstring>
 using namespace std;
 fstream fin1, fin2, fileout;
 char nome1[16];
@@ -16,20 +17,21 @@ while (*s1) {*s = *s1; s++; s1++;};
     while (*s2) {*s = *s2; s++; s2++;};
     *s = '\0';
     return cat;
-
 }
 
 // questa funzione tenta di leggere una stringa s da fin
 // e restituisce true solo se ci riesce
 bool LeggiStringa(fstream& fin, char* s) {
-
+if(fin>>s) return true;
+else return false;
 }
 
 // questa funzione copia la parola s nel filestream fout e quindi chiama
 // la funzione LeggiStringa per tentare di leggere un’altra stringa dal
 // filestream fin; restituisce true solo se ci riesce.
 bool CopiaStringa(fstream& fout, fstream& fin, char* s) {
-
+fout<<" "<<s;
+return LeggiStringa(fin,s);
 }
 
 // questo programma chiede i nomi dei due file da fondere ordinatamente
@@ -39,24 +41,40 @@ bool CopiaStringa(fstream& fout, fstream& fin, char* s) {
 // utilizza le funzioni CopiaStringa, LeggiStringa e strcmp
 int main() {
 
+/*
     cout<<"Inserire nome file 1:"<<endl;
     cin>>nome1;
 
     cout<<"Inserire nome file 2:"<<endl;
     cin>>nome2;
+*/
 
-    fin1.open(nome1,ios::in);
+    fin1.open("C:\\Users\\Thinkpad User\\CLionProjects\\concatenazione\\dizio.txt",ios::in);
     if(!fin1.is_open()) cout<<"Errore apertura "<<nome1<<endl;
 
-    fin2.open(nome2,ios::in);
+    fin2.open("C:\\Users\\Thinkpad User\\CLionProjects\\concatenazione\\nario.txt",ios::in);
     if(!fin2.is_open()) cout<<"Errore apertura "<<nome2<<endl;
-
+/*
     char *nome3[32];
     *nome3 = concatena(nome1, nome2);
     cout<<"Concat: "<<*nome3<<endl;
+*
+ */
+    fileout.open("C:\\Users\\Thinkpad User\\CLionProjects\\concatenazione\\dizionario.txt",ios::out);
+    if(!fileout.is_open()) cout<<"Errore apertura "<<endl;//*nome3<<endl;
 
-    fileout.open(*nome3,ios::out);
-    if(!fileout.is_open()) cout<<"Errore apertura "<<*nome3<<endl;
+    char p1[30], p2[30];
+    bool sx=LeggiStringa(fin1,p1);
+    bool dx=LeggiStringa(fin2,p2);
 
+    while(sx&&dx)
+        if(strcmp(p1,p2)<=0) sx=CopiaStringa(fileout,fin1,p1);
+        else dx=CopiaStringa(fileout,fin2,p2);
 
+        fileout<<endl;
+        fin1.close();
+        fin2.close();
+        fileout.close();
+
+        return 0;
 }
